7ae7ae02420657f1371df466881aef1c4bf2b178
Gerardo <gerardopapaleco@gmail.com>
2020-09-30 22:20:44 -0400

    Added Functions

--- Files/CalculadoraRPN_ver_1.c
+++ Files/CalculadoraRPN_ver_1.c
@@ -3,7 +3,7 @@
 #include <ctype.h>
 #include <math.h>
 #include <windows.h>
-typedef float TData;
+typedef double TData;
 int i, check;
 void gotoxy(int x, int y)
 {
@@ -60,7 +60,7 @@ void MostrarPila(struct nodo **AnteriorP)
     {
         while (Actual != NULL)
         {
-            printf("\n%.2f", Actual->digito);
+            printf("\n%.3f", Actual->digito);
             Actual = Actual->siguiente;
         }
     }
@@ -93,18 +93,33 @@ int CheckArgumentos(struct nodo **AnteriorP)
 {
     if ((PilaVacia(*AnteriorP)) || (Actual->siguiente == NULL))
     { //Minimo se necesitan 2 valores.
-        gotoxy(30, 7);
-        printf("Pocos argumentos.");
-        gotoxy(30, 8);
-        printf("Recuede la notacion RPN. Ej: (2 4 +) ");
+        gotoxy(30, 10);
+        printf("Few Arguments!");
+        gotoxy(30, 11);
+        printf("Remember the Reverse Polish Notation. Ex: (2 -> 4 -> +)");
         system("pause > NULL");
         system("cls");
         return 1;
     }
-    else
+    return 0;
+}
+int CheckArgumentosSingle(struct nodo **AnteriorP){
+    if (PilaVacia(*AnteriorP))
     {
-        return 0;
+        gotoxy(30, 10);
+        printf("Few Arguments!");
+        system("pause > NULL");
+        system("cls");
+        return 1;
     }
+    return 0;
+}
+int Factorial(TData digito){
+    if (digito != 1)
+    {
+        return digito * Factorial(digito - 1);
+    }
+    return digito;
 }
 //Operaciones de la calculadora
 void Evaluacion(struct nodo **AnteriorP, char op)
@@ -152,24 +167,52 @@ void Evaluacion(struct nodo **AnteriorP, char op)
         system("cls");
         return;
     case 'n':
-        if (PilaVacia(*AnteriorP))
+        if (CheckArgumentosSingle(AnteriorP))
         {
-            gotoxy(30, 7);
-            printf("Pocos Argumentos.");
-            system("pause > NULL");
-            system("cls");
             return;
         }
         resultado = Actual->digito * (-1);
-        Actual = *AnteriorP;
+        *AnteriorP = Actual->siguiente;
+        free(Actual);
+        IngresarDigito(AnteriorP, resultado);
+        system("cls");
+        return;
+    case '!':
+        if (CheckArgumentosSingle(AnteriorP))
+        {
+            return;
+        }
+        resultado = Factorial(Actual->digito);
+        *AnteriorP = Actual->siguiente;
+        free(Actual);
+        IngresarDigito(AnteriorP, resultado);
+        system("cls");
+        return;
+    case 'i':
+        if (CheckArgumentosSingle(AnteriorP))
+        {
+            return;
+        }
+        resultado = 1 / Actual->digito;
+        *AnteriorP = Actual->siguiente;
+        free(Actual);
+        IngresarDigito(AnteriorP, resultado);
+        system("cls");
+        return;
+    case '%':
+        if (CheckArgumentosSingle(AnteriorP))
+        {
+            return;
+        }
+        resultado = Actual->digito / 100;
         *AnteriorP = Actual->siguiente;
         free(Actual);
         IngresarDigito(AnteriorP, resultado);
         system("cls");
         return;
     default:
-        gotoxy(30, 7);
-        printf("Operador erroneo.");
+        gotoxy(30, 10);
+        printf("Wrong Operator");
         system("pause > NULL");
         system("cls");
         return;
@@ -184,19 +227,23 @@ void Evaluacion(struct nodo **AnteriorP, char op)
 void Leyenda()
 {
     gotoxy(30, 0);
-    printf("'c' = Limpiar calculadora.");
+    printf("'c' = Clear all the stack");
     gotoxy(30, 1);
-    printf("'n' = Multipicar el ultimo numero ingresado por -1");
+    printf("'n' = Turn last number into its oppossite ( n * -1 )");
     gotoxy(30, 2);
-    printf("'e' = Salir");
+    printf("'e' = Exit");
+    gotoxy(30, 3);
+    printf("'i' = 1/(lastNumber)");
     gotoxy(30, 4);
-    printf("Gerardo Torres");
-    gotoxy(30, 5);
-    printf("Copyright 2020 (actually no) yes");
+    printf("'%c' = (lastNumber)/100", 37);
+    gotoxy(30, 8);
+    printf("Gerardo Torres @gistypical");
 }
 int main(int argc, char const *argv[])
 {
     CrearPila(&AnteriorP);
+    gotoxy(30, 5);
+    printf("How to: Write a digit an then hit 'enter' for pushing into the stack\n");
     do
     {
         check = 1;
@@ -211,8 +258,8 @@ int main(int argc, char const *argv[])
                 operador = datos[0]; //Se consigue un operador y se sale del while.
                 break;
             }
-            temporal = atof(datos); // Si se detecta que es un digito, se pasa a float.
-            IngresarDigito(&AnteriorP, temporal);
+            // temporal = atof(datos); // Si se detecta que es un digito, se pasa a float.
+            IngresarDigito(&AnteriorP, atof(datos));
             system("cls");
             MostrarPila(&AnteriorP);
         }
